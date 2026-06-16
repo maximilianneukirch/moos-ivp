b58ebbb466555829838ce34bcc38b1e072525baa
Maximilian von Neukirch <maximilian.neukirch@uni-konstanz.de>
2026-06-16 17:06:16 +0200

    current configuration of visflocking_test using new BHV_AvoidCollisionSail behavior

--- ivp/missions/visflocking_test/launch.sh
+++ ivp/missions/visflocking_test/launch.sh
@@ -44,7 +44,7 @@ for ((i=1; i<=$VEHICLE_COUNT; i++)); do
   MOOS_PORT=$((9000 + $i))
   PSHARE_PORT=$((9200 + $i))
   #POLAR_PLOT_STR="0,100: 90,100: 180,100"
-  POLAR_PLOT_STR="0,0: 45,100: 90,60: 135,100: 180,0"
+  POLAR_PLOT_STR="0,0: 30,40: 45,80: 90,90: 135,100: 180,60"
 
   # Generate .moos and .bhv files
   nsplug meta_vehicle.moos "targ_${VNAME}.moos" -f VNAME="$VNAME" MOOS_PORT="$MOOS_PORT" PSHARE_PORT="$PSHARE_PORT" POLAR_PLOT="$POLAR_PLOT_STR" START_POS="x=$(($i*6)),y=$((-20 + $i%3)),heading=-$(($i*10))"
