7302022e4d3a83c934a20005400daddea774bb57
Maximilian von Neukirch <maximilian.neukirch@uni-konstanz.de>
2026-07-06 13:41:33 +0200

    Adding uNodeReportDistributor to unpack condensed NODE_REPORTS on target boat

---
+++ ivp/src/uNodeReportDistributor/NodeReportDistributor.cpp
@@ -0,0 +1,109 @@
+#include "NodeReportDistributor.h"
+#include "MBUtils.h"
+#include <vector>
+
+using namespace std;
+
+//---------------------------------------------------------
+// Constructor
+NodeReportDistributor::NodeReportDistributor() {
+    m_extra_args = ""; 
+}
+
+//---------------------------------------------------------
+// Procedure: OnStartUp()
+bool NodeReportDistributor::OnStartUp() {
+    STRING_LIST sParams;
+    m_MissionReader.EnableVerbatimQuoting(false);
+    
+    if(m_MissionReader.GetConfiguration(GetAppName(), sParams)) {
+        STRING_LIST::iterator p;
+        for(p = sParams.begin(); p != sParams.end(); p++) {
+            string line  = *p;
+            string param = tolower(biteStringX(line, '='));
+            string value = line;
+
+            if(param == "extra_args") {
+                m_extra_args = value;
+            }
+        }
+    }
+    
+    RegisterVariables();
+    return true;
+}
+
+//---------------------------------------------------------
+// Procedure: OnConnectToServer()
+bool NodeReportDistributor::OnConnectToServer() {
+    RegisterVariables();
+    return true;
+}
+
+//---------------------------------------------------------
+// Procedure: RegisterVariables()
+void NodeReportDistributor::RegisterVariables() {
+    m_Comms.Register("NODE_REPORTS_AGG", 0);
+}
+
+//---------------------------------------------------------
+// Procedure: OnNewMail()
+bool NodeReportDistributor::OnNewMail(MOOSMSG_LIST &NewMail) {
+    MOOSMSG_LIST::iterator p;
+    for(p = NewMail.begin(); p != NewMail.end(); p++) {
+        CMOOSMsg &msg = *p;
+        string key = msg.GetKey();
+
+        if(key == "NODE_REPORTS_AGG" && msg.IsString()) {
+            string agg_string = msg.GetString();
+            
+            // Split at '|'
+            vector<string> boat_records = parseString(agg_string, '|');
+            
+            for(unsigned int i = 0; i < boat_records.size(); i++) {
+                string record = boat_records[i];
+                
+                // vname,x,y,hdg
+                vector<string> fields = parseString(record, ',');
+                
+                if(fields.size() == 4) {
+                    string vname = fields[0];
+                    string vx    = fields[1];
+                    string vy    = fields[2];
+                    string vhdg  = fields[3];
+                    
+                    // standard MOOS NODE_REPORT string
+                    string node_report = "NAME=" + vname + 
+                                         ",X=" + vx + 
+                                         ",Y=" + vy + 
+                                         ",HDG=" + vhdg + 
+                                         ",TIME=" + doubleToStringX(MOOSTime());
+                                         
+                    if(!m_extra_args.empty()) {
+                        node_report += "," + m_extra_args;
+                    }
+
+                    m_Comms.Notify("NODE_REPORTTTT", node_report);
+                }
+            }
+        }
+    }
+    return true;
+}
+
+//---------------------------------------------------------
+// Procedure: Iterate()
+bool NodeReportDistributor::Iterate() {
+    // Empty, since everything is handled in OnNewMail
+    return true;
+}
+
+//------------------------------------------
+// uNodeReportDistributor config block
+//ProcessConfig = uNodeReportDistributor
+//{
+//  AppTick   = 4
+//  CommsTick = 4
+//  
+//  extra_args = TYPE=kayak,LENGTH=1.1
+//}
---
+++ ivp/src/uNodeReportDistributor/NodeReportDistributor.h
@@ -0,0 +1,23 @@
+#ifndef NODE_REPORT_DISTRIBUTOR_H
+#define NODE_REPORT_DISTRIBUTOR_H
+
+#include "MOOS/libMOOS/MOOSLib.h"
+#include <string>
+
+class NodeReportDistributor : public CMOOSApp {
+public:
+    NodeReportDistributor();
+    ~NodeReportDistributor() {};
+
+protected:
+    bool OnNewMail(MOOSMSG_LIST &NewMail);
+    bool Iterate();
+    bool OnConnectToServer();
+    bool OnStartUp();
+    void RegisterVariables();
+
+private:
+    std::string m_extra_args;
+};
+
+#endif
